4f6b6c69a1476e1fb46192290c8316db63afc573
mehadi1720 <mehedihasanrana.17203087@gmail.com>
2021-11-09 22:44:30 +0600

    problem solve

---
+++ day3.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+ int N;
+ cin>>N;
+ if(N%2==0)
+ {
+  if(N>=2 && N<=5)
+   {
+    cout<<"Not Weird";
+   }
+   else if(N>=6 && N<=20)
+   {
+    cout<<"Weird";
+   }
+   else
+    {
+    cout<<"Not Weird";
+   }
+  }
+
+ else
+  {
+  cout<<"Weird";
+
+}
+}
