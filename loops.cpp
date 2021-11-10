2e379856ba117ebd37b695de23b489a1ca1a6c6e
mehadi1720 <mehedihasanrana.17203087@gmail.com>
2021-11-10 12:35:00 +0600

    init repo

---
+++ loops.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+using namespace std;
+ int main()
+ {
+  int n;
+  int p;
+  cin>>n;
+  for(int i=1;i<=10;i++)
+  {
+
+   p=n*i;
+   cout<<n<<" X "<<i<<" = "<<p<<endl;
+  }
+
+
+ }
